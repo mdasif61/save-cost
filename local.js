53afad199528e555a15117999c2e6a826ced52e3
Ikbal Hossain Asif <asifikbalgfh312@gmail.com>
2023-03-14 19:32:12 +0600

    bank of history

--- local.js
+++ local.js
@@ -29,7 +29,7 @@ function showItem(){
         const {itemText,random}=local[i]
         show +=`
     <div class="bg-gray-200 font-semibold text-lg py-3 px-5 m-3 text-black flex items-center justify-between flex-col md:flex-row">
-        <p class="text-sm hidden md:block">${i}/</p>
+        <p class="text-sm hidden md:block">${i}.</p>
         <p class="text-sm">${itemText}</p>
         <p class="text-sm text-gray-600">${new Date().getDate()}/${new Date().getMonth()+1}/${new Date().getFullYear()}</p>
         
